a4254acf8d6cf26137b9ee8722f0da3c2acfa00d
burakcersit <burakcersit@hotmail.com>
2023-01-15 14:32:53 -0500

    syntax error

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
+      //'mongodb://localhost:27017/socialnetwork', {
+
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialnetwork",
   {
-    //'mongodb://localhost:27017/socialmedia', {
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
